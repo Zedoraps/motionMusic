391657a49ee4c53448d23c448c7cdf8e5fb73bf6
Zedoraps <philip@moesli.net>
2022-07-17 01:50:15 +0200

    🚎🚎

--- app.py
+++ app.py
@@ -1,43 +1,18 @@
-import time
-from datetime import datetime, timedelta
-
 from flask import Flask
 
-from motion import Motion
-from player import Player
+from manager import Manager
 
 app = Flask(__name__)
+manager = Manager()
 
 
-@app.route('/')
+@app.route('/start')
 def index():
-    start_player()
-    return 'Index Page'
-
-
-stop_time = datetime.now()
+    return manager.start_player()
 
 
 def main():
     app.run()
-    start_player()
-
-
-def start_player():
-    player = Player("/home/croc/Desktop/croc/songs")
-
-    def on_motion():
-        player.play_or_continue_playing()
-        global stop_time
-        stop_time = datetime.now() + timedelta(seconds=10)
-
-    sensor = Motion(on_motion)
-    while True:
-        time.sleep(1)
-        print(f'{datetime.now().strftime("%H:%M:%S")}: {player.info()} end at: {stop_time.strftime("%H:%M:%S")}')
-        if stop_time < datetime.now():
-            print("End time reached")
-            player.stop()
 
 
 if __name__ == "__main__":
---
+++ manager.py
@@ -0,0 +1,36 @@
+import time
+from datetime import datetime, timedelta
+from threading import Thread
+
+from motion import Motion
+from player import Player
+
+
+class Manager:
+    sensor_thread = None
+
+    def __init__(self):
+        self.stop_time = datetime.now()
+        self.player = Player("/home/croc/Desktop/croc/songs")
+        self.sensor = Motion(self.on_motion)
+
+    def start_player(self):
+        if self.sensor_thread is not None:
+            return "Already running"
+        else:
+            sensor_thread = Thread(target=self.unsave_start_player())
+            sensor_thread.start()
+            return 'Started'
+
+    def on_motion(self):
+        self.player.play_or_continue_playing()
+        self.stop_time = datetime.now() + timedelta(seconds=10)
+
+    def sens(self):
+        while True:
+            time.sleep(1)
+            print(
+                f'{datetime.now().strftime("%H:%M:%S")}: {self.player.info()} end at: {self.stop_time.strftime("%H:%M:%S")}')
+            if self.stop_time < datetime.now():
+                print("End time reached")
+                self.player.stop()
