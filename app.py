c1ef36592279a4cf6b5d0d334b6fe9551d666284
Zedoraps <philip@moesli.net>
2022-07-16 19:37:15 +0200

    🏎🏎

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from datetime import datetime
 from motion import Motion
 
 
+def on_motion():
+    print("Would start playing now!")
+
+
 def main():
-    sensor = Motion()
+    sensor = Motion(on_motion)
     while True:
         time.sleep(1)
         print(f'{datetime.now().strftime("%H:%M:%S")}: {sensor.motion}')
--- motion.py
+++ motion.py
@@ -4,14 +4,16 @@ from gpiozero import MotionSensor
 
 class Motion:
 
-    def __init__(self):
+    def __init__(self, method):
         self.motion = False
         self.sens = MotionSensor(4)
+        self.method = method
 
         self.sens.when_motion = self.on_motion
         self.sens.when_no_motion = self.on_no_motion
 
     def on_motion(self):
+        self.method()
         self.motion = True
         print(f'{datetime.now().strftime("%H:%M:%S")}: Motion Detected')
 
